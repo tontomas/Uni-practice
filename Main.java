9cc54a00f59b99beac31ac7f22d1d96b9f3d7ebe
Tomás García <togarcial@unal.edu.co>
2025-05-12 00:09:45 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Porfavor, ingresa un número: ");
+        int multiplos = sc.nextInt();
+        System.out.print("Porfavor ingresa la cantidad de múltiplos: ");
+        int multiplicando = sc.nextInt();
+        int sumaTotal = 0;
+        
+        for (int i = 1; i <= multiplicando; i++) {
+            int resultado = i * multiplos;
+            sumaTotal += resultado;
+            System.out.println(multiplos + " * " + i + " = " + resultado );
+        }
+        
+        System.out.print("Suma total: " + sumaTotal);
+    }
+}
